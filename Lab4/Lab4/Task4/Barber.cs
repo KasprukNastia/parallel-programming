b5c3d933704a37b5c8b234bf1b75412f8da6fa79
Настя Каспрук <anastasiyakaspruk17@gmail.com>
2020-10-09 10:54:02 +0300

    continued

--- Lab4/Lab4/Task4/Barber.cs
+++ Lab4/Lab4/Task4/Barber.cs
@@ -21,18 +21,21 @@ namespace Lab4.Task4
             {
                 while (!CancellationTokenSource.IsCancellationRequested)
                 {
-                    _barberShop.ClientsSemaphore.WaitOne();
-                    if (_barberShop.Clients.Count == 0)
+                    _barberShop.SeatsSemaphore.WaitOne();
+                    if (_barberShop.TakenSeats == 0)
                     {
-                        _barberShop.ClientsSemaphore.Release();
-                        _barberShop.BarberSemaphore.WaitOne();
-                        _barberShop.ClientsSemaphore.WaitOne();                     
+                        _barberShop.SeatsSemaphore.Release();
+                        _barberShop.BarberSemaphore.Release();
+                        _barberShop.SeatsSemaphore.WaitOne();
                     }
 
+                    _barberShop.ClientsSemaphore.WaitOne();
                     Client client = _barberShop.Clients.Dequeue();
+                    _barberShop.TakenSeats--;
                     Console.WriteLine($"{DateTime.Now.ToString("hh:mm:ss.fff")}: Barber serves the client with id '{client.ClientId}' for {client.HaircutDuration} ms");
                     Thread.Sleep(client.HaircutDuration);
                     _barberShop.ClientsSemaphore.Release();
+                    _barberShop.SeatsSemaphore.Release();
                 }
             }, CancellationTokenSource.Token);
         }
--- Lab4/Lab4/Task4/Client.cs
+++ Lab4/Lab4/Task4/Client.cs
@@ -20,20 +20,31 @@ namespace Lab4.Task4
         {
             return Task.Run(() =>
             {
-                _barberShop.ClientsSemaphore.WaitOne();
-                if (_barberShop.Clients.Count < _barberShop.MaxClientsCount)
+                _barberShop.SeatsSemaphore.WaitOne();
+                if (_barberShop.TakenSeats < _barberShop.MaxClientsCount)
                 {
-                    Console.WriteLine($"The number of taken seats is {_barberShop.Clients.Count} so new client with id '{ClientId}' WILL be added to the queue");       
+                    Console.WriteLine($"The number of taken seats is {_barberShop.TakenSeats} so new client with id '{ClientId}' WILL be added to the queue");
+                    
+                    _barberShop.ClientsSemaphore.WaitOne();
                     _barberShop.Clients.Enqueue(this);
+                    _barberShop.TakenSeats++;
+                    _barberShop.ClientsSemaphore.Release();
 
                     if (_barberShop.Clients.Count == 1)
-                        _barberShop.BarberSemaphore.Release();
+                    {
+                        _barberShop.SeatsSemaphore.Release();
+                        _barberShop.BarberSemaphore.WaitOne();
+                    }
+                    else
+                    {
+                        _barberShop.SeatsSemaphore.Release();
+                    }          
                 }
                 else
                 {
-                    Console.WriteLine($"Queue is filled. The number of taken seats is {_barberShop.Clients.Count} so new client with id '{ClientId}' WILL NOT be added to the queue");
+                    Console.WriteLine($"Queue is filled. The number of taken seats is {_barberShop.TakenSeats} so new client with id '{ClientId}' WILL NOT be added to the queue");
+                    _barberShop.SeatsSemaphore.Release();
                 }
-                _barberShop.ClientsSemaphore.Release();
             });
         }
     }
